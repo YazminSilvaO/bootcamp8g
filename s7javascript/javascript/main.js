f1c9941147827adcfea6096674c285e0adc40b3c
yazmin.9211@gmail.com <yazmin.9211@gmail.com>
2020-05-25 23:07:30 -0500

    Tarea de JS

---
+++ s7javascript/javascript/main.js
@@ -0,0 +1,14 @@
+var numberOne = 10
+var numberTwo = 5
+var additionResult = numberOne + numberTwo
+var substractionResult = numberOne -  numberTwo
+var multiplicationResult= numberOne * numberTwo
+var divsionResult= numberOne / numberTwo
+var moduloResult= numberOne % numberTwo
+
+
+console.log("La suma de 10 + 5 es igual a", additionResult)
+console.log("La resta de 10 - 5 es igual a", substractionResult)
+console.log("La multiplicación de 10 * 5 es igual a", multiplicationResult)
+console.log("La división de 10 / 5 es igual a", divsionResult)
+console.log("10 % de 5 es igual a", moduloResult)
