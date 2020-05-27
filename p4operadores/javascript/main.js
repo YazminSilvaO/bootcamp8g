4a7dca17b2b8c9b011923ee9080d736b32a20b02
yazmin.9211@gmail.com <yazmin.9211@gmail.com>
2020-05-26 23:18:54 -0500

    Practica Operadores

---
+++ p4operadores/javascript/main.js
@@ -0,0 +1,26 @@
+var anyNumber = 5
+
+if (anyNumber %0){
+    console.log("La variable es par")
+}else{
+    console.log("La variable es impar")
+}
+
+
+var base = 10
+var height = 10
+
+if (base === height){
+    console.log("La base y la altura son iguales, entonces se hace un cuadrado")
+}else{
+    console.log("La base y la altura son desiguales, entonces se hace un rectangulo")
+}
+
+var side = 6
+
+if (side ==3 && side <= 5){
+    console.log("Tu figura puede ser un triangulo, un cuadrilatero o un pentágono")
+}else{ 
+    console.log("Tu figura tiene muchos lados")
+
+}
